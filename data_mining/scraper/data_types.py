d02ff55988326c623673a6a19e50883919fb34e4
Curtis Wirtz <supcurtis@gmail.com>
2025-07-08 00:20:08 -0700

    scraper: conditional fallbacks when fighter fields are empty or not found on page

--- data_mining/scraper/data_types.py
+++ data_mining/scraper/data_types.py
@@ -20,12 +20,8 @@ def search_by_class(class_name, bs4_document):
     field = bs4_document.find(class_=class_name).get_text(strip=True)
     return field
 
-def search_by_shared_class(class_name, bs4_document):
-    field = bs4_document.find(class_=class_name).get_text(strip=True)
-    return field
-
 def fighter_page(hyperlink):
-    with open("index.html", "r") as f: # r for read mode
+    with open("incomplete.html", "r") as f: # r for read mode
         doc = BeautifulSoup(f, "html.parser")
 
     # result = requests.get(hyperlink)
@@ -33,17 +29,44 @@ def fighter_page(hyperlink):
 
     fighter_id = urlparse(hyperlink).path.split('/')[-1]  # Extract the last part of the URL
     name = search_by_class('b-content__title-highlight', doc)
-    nickname = search_by_class('b-content__Nickname', doc)
+
+    if doc.find('p', class_='b-content__Nickname').get_text(strip=True) == "":
+        nickname = None
+    else:
+        nickname = search_by_class('b-content__Nickname', doc)
+    
     record = search_by_class('b-content__title-record', doc)
     wins = int(record.split('-')[0].strip()[8:])
     losses = int(record.split('-')[1].strip())
     draws = int(record.split('-')[2].strip())
+
     table_stats = doc.find_all('li', class_='b-list__box-list-item b-list__box-list-item_type_block')
-    height = table_stats[0].get_text(strip=True).split(':')[1]
-    weight = int(table_stats[1].get_text(strip=True).split(':')[1][:-5])  # Remove 'lbs' from the end
-    reach = table_stats[2].get_text(strip=True).split(':')[1].strip()
-    stance = table_stats[3].get_text(strip=True).split(':')[1].strip()
-    date_of_birth = table_stats[4].get_text(strip=True).split(':')[1].strip()
+
+    if (table_stats[0].get_text(strip=True).split(':')[1] == "--" or ""):
+        height = None
+    else:
+        height = table_stats[0].get_text(strip=True).split(':')[1]
+
+    if (table_stats[1].get_text(strip=True).split(':')[1] == "--" or ""):
+        weight = None
+    else:
+        weight = int(table_stats[1].get_text(strip=True).split(':')[1][:-5])  # Remove 'lbs' from the end
+    
+    if (table_stats[2].get_text(strip=True).split(':')[1].strip() == "--" or ""):
+        reach = None
+    else:
+        reach = table_stats[2].get_text(strip=True).split(':')[1].strip()
+
+    if (table_stats[3].get_text(strip=True).split(':')[1].strip() == ""):
+        stance = None
+    else:
+        stance = table_stats[3].get_text(strip=True).split(':')[1].strip()
+
+    if (table_stats[4].get_text(strip=True).split(':')[1].strip() == "--" or ""):
+        date_of_birth = None
+    else:
+        date_of_birth = table_stats[4].get_text(strip=True).split(':')[1].strip()
+
 
     fighter_data = {
         'fighter_id': fighter_id,  # Primary Key
