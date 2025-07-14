6b57d5ceee2fb5ad18fcbd61d2f331348cfff995
Curtis Wirtz <supcurtis@gmail.com>
2025-07-14 16:25:59 -0700

    django admin adjustments, proper fields selected for more desired display

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -5,15 +5,15 @@ from .models import Fighter, Event, Bout
 
 class FighterAdmin(admin.ModelAdmin):
     list_display = ["name", "nickname", "wins", "losses", "draws", "weight_lb"]
-    search_fields = ["name", "nickname", "weight_lb", "date_of_birth"]
+    search_fields = ["fighter_id", "name", "nickname", "weight_lb", "date_of_birth"]
 admin.site.register(Fighter, FighterAdmin)
 
 class EventAdmin(admin.ModelAdmin):
     list_display = ["name", "date", "location"]
-    search_fields = ["name", "date", "location"]
+    search_fields = ["event_id", "name", "date", "location"]
 admin.site.register(Event, EventAdmin)
 
 class BoutAdmin(admin.ModelAdmin):
     list_display = ["bout_id", "fighter_1", "fighter_2", "event", "event__date"]
-    search_fields = ["fighter_1__name", "fighter_2__name", "event__name", "result"]
+    search_fields = ["bout_id", "fighter_1__name", "fighter_2__name", "event__name", "result"]
 admin.site.register(Bout, BoutAdmin)
