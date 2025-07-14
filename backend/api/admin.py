aae521ffd9e417958e3144434187db83767badff
Curtis Wirtz <supcurtis@gmail.com>
2025-07-13 23:54:46 -0700

    register/organize events, bouts, and fighters to django admin panel

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Fighter, Event, Bout
+
+
+class FighterAdmin(admin.ModelAdmin):
+    list_display = ["name", "nickname", "wins", "losses", "draws", "weight_lb"]
+    search_fields = ["name", "nickname", "weight_lb", "date_of_birth"]
+admin.site.register(Fighter, FighterAdmin)
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ["name", "date", "location"]
+    search_fields = ["name", "date", "location"]
+admin.site.register(Event, EventAdmin)
+
+class BoutAdmin(admin.ModelAdmin):
+    list_display = ["bout_id", "fighter_1", "fighter_2", "event", "event__date"]
+    search_fields = ["fighter_1__name", "fighter_2__name", "event__name", "result"]
+admin.site.register(Bout, BoutAdmin)
