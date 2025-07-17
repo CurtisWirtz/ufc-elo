5544d67cde4f80875b0e3358c50282ca66e5ffdd
Curtis Wirtz <supcurtis@gmail.com>
2025-07-16 17:38:06 -0700

    inside urls.py urlpaths: add auth routes and provide router structure for views to data types that will be created

--- backend/ufc/urls.py
+++ backend/ufc/urls.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
-from api.views import CreateUserView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
+# from api.views import FighterViewSet, EventViewSet, BoutViewSet
+
+# router = DefaultRouter()
+# router.register(r'fighters', FighterViewSet)
+# router.register(r'events', EventViewSet)
+# router.register(r'bouts', BoutViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/user/register/", CreateUserView.as_view(), name="register"),
-    path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh_token"),
-    path("api-auth/", include("rest_framework.urls")),
+    # path('api/', include(router.urls)),
+
+    # JWT auth endpoints
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+    # PRODUCTION_XXX nice for developement, but I'll strip this out for production
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
