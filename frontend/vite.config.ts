ca1c10cb76279adf3566864003945cafb63ed25a
Curtis Wirtz <supcurtis@gmail.com>
2025-07-16 21:14:00 -0700

    configure vite's proxy to the django backend inside vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,13 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  server: {
+    port: 5173, // Default Vite port
+    proxy: {
+      '/api': { // if a request starts with /api
+        target: 'http://127.0.0.1:8000', // proxy it to the django backend
+        changeOrigin: true, // this is neeeded for virtual hosts
+      },
+    },
+  },
 });
