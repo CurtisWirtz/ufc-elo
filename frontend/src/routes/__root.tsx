f5bd350e8b433625999002019a5774a78ab4ce76
Curtis Wirtz <supcurtis@gmail.com>
2025-07-23 20:54:32 -0700

    basic configuration of a header with some links to events and fighters pages

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,10 +1,28 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { Outlet, Link, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
 export const Route = createRootRoute({
   component: () => (
     <>
+      <div className="p-2 flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>{' '}
+        <Link to="/events" className="[&.active]:font-bold">
+          Events
+        </Link>
+        <Link to="/fighters" className="[&.active]:font-bold">
+          Fighters
+        </Link>
+        <Link to="/search" className="[&.active]:font-bold">
+          Search
+        </Link>
+      </div>
+      <hr />
+      
       <Outlet />
+
+      {/* Don't use in production */}
       <TanStackRouterDevtools />
     </>
   ),
