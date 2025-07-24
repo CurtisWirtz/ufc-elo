c05ef3094a0ec6771ed8f26e8130650ad6cdca1b
Curtis Wirtz <supcurtis@gmail.com>
2025-07-23 22:52:39 -0700

    break types into separate files inside types folder

--- frontend/src/types/bout.types.ts
+++ frontend/src/types/bout.types.ts
@@ -1,41 +1,5 @@
-export interface Event {
-    event_id: string;
-    name: string;
-    date: string;
-    location: string;
-    bout_order: string[];
-}
-    // event_id = models.CharField(max_length=64, primary_key=True)
-    // name = models.CharField(max_length=255)
-    // date = models.DateField()
-    // location = models.CharField(max_length=255)
-    // bout_order = ArrayField(models.CharField(max_length=64), blank=True, default=list)
-
-
-export interface Fighter {
-    fighter_id: string;
-    name: string;
-    nickname?: string;
-    wins: number;
-    losses: number;
-    draws: string;
-    height_in?: number;
-    weight_lb?: number;
-    reach_in?: number;
-    stance?: string;
-    date_of_birth?: string;
-}
-    // name = models.CharField(max_length=255)
-    // nickname = models.CharField(max_length=255, null=True, blank=True)
-    // wins = models.IntegerField(default=0)
-    // losses = models.IntegerField(default=0)
-    // draws = models.CharField(max_length=64, default="0")
-    // height_in = models.IntegerField(null=True, blank=True)
-    // weight_lb = models.IntegerField(null=True, blank=True)
-    // reach_in = models.IntegerField(null=True, blank=True)
-    // stance = models.CharField(max_length=64, null=True, blank=True)
-    // date_of_birth = models.DateField(null=True, blank=True)
-
+import type { Event } from './event.types.ts';
+import type { Fighter } from './fighter.types.ts';
 
 export interface Bout {
     bout_id: string;
