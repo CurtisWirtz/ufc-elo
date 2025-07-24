263286130bd4734e4dfac63c7d25249a9c6f8544
Curtis Wirtz <supcurtis@gmail.com>
2025-07-24 03:32:14 -0700

    add note and create user serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,14 +1,22 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-from .models import Event, Fighter, Bout
+from .models import Note, Event, Fighter, Bout
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "username", "password"]
-        extra_kwargs = {"password": {"write_only": True}} # tells django accept a password, but dont return one when giving info about a user.
+        extra_kwargs = {"password": {"write_only": True}} # write_only: accept a password, but dont return one when giving info about a user.
 
-    def create(self, validated_data): # if the Meta fields are valid, this method will be called to create a new user.
+    # if the Meta fields are valid, this method will be called to create a new user.
+    def create(self, validated_data): 
+        print("Creating user with data:", validated_data)
         user = User.objects.create_user(**validated_data)
-        return user
+        return user 
+    
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = {"id", "title", "content", "created_at", "author"}
+        extra_kwargs = {"author": {"read_only": True}}  # see the author field, but dont allow setting author.. its auto assigned.
