cfb3fcfb8d24d3f860889315557bbf59bac8a986
Curtis Wirtz <supcurtis@gmail.com>
2025-07-23 20:21:43 -0700

    install frontend: vite@latest with tanstack-router, react-query, axios, tailwind, typescript, react and tanstack devtools

---
+++ frontend/src/main.tsx
@@ -0,0 +1,41 @@
+import React, { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import './styles.css'
+
+const queryClient = new QueryClient()
+
+// Import the generated route tree
+import { routeTree } from './routeTree.gen'
+
+// Create a new router instance
+const router = createRouter({ 
+  routeTree,
+  context: { queryClient } // Pass the query client to the router context
+})
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+// Render the app
+const rootElement = document.getElementById('root')!
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        
+        {/* Don't use in production */}
+        <ReactQueryDevtools initialIsOpen={false} />
+
+      </QueryClientProvider>
+    </StrictMode>
+  )
+}
