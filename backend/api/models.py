09aa88a8d347046ef3e004ddacf24f56dad96543
Curtis Wirtz <supcurtis@gmail.com>
2025-07-24 03:32:32 -0700

    add model: Note and make migrations

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
+
+class Note(models.Model):
+    title = models.CharField(max_length=64)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
+
+    def __str__(self):
+        return self.title
 
 class Event(models.Model):
     event_id = models.CharField(max_length=64, primary_key=True)
