d70eea7c4372b9f35c65362783334cb260741cef
Curtis Wirtz <supcurtis@gmail.com>
2025-07-23 23:32:15 -0700

    fix: tanstack router documentation suggests not separating a Route from its' Component into separate files without good reason

--- frontend/src/routes/events/$eventId.tsx
+++ frontend/src/routes/events/$eventId.tsx
@@ -1,6 +1,24 @@
-import { createFileRoute } from '@tanstack/react-router'
-import EventPage from './_components/EventPage.tsx'
+import { Link, createFileRoute } from '@tanstack/react-router'
+import { getEventById } from '../../api/queries.ts'
 
 export const Route = createFileRoute('/events/$eventId')({
   component: EventPage,
 })
+
+function EventPage() {
+  const { eventId } = Route.useParams();
+  const { data: event, isLoading, isError } = getEventById(eventId);
+
+  return (
+    <>
+      <Link to={`/events`}>Back to Events</Link>
+      <div>Hello "/events/{eventId}"!</div>
+      {
+        isLoading ? <h1>Loading...</h1> : isError ? <h1>Error fetching events</h1> : (
+          event &&
+          <div className={`mb-2 text-xl`}>{event.title}</div>
+        )
+      }
+    </>
+  );
+}
