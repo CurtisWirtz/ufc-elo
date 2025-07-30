7cdd5bc13fa8a2131a062b69cc5e13c918f475aa
Curtis Wirtz <supcurtis@gmail.com>
2025-07-29 17:39:56 -0700

    ensure that /api/ path is included in api request, to correctly hit events enpoints

--- frontend/src/api/queries.ts
+++ frontend/src/api/queries.ts
@@ -5,10 +5,10 @@ import { api } from './client.ts';
 
 // Function to fetch all events
 export const getEvents = async (): Promise<AxiosResponse<Event[], any>> => {
-    return await api.get<Event[]>('/events');
+    return await api.get<Event[]>('/api/events');
 };
 
 // Function to fetch a single event by ID
 export const getEventById = async (id: string): Promise<AxiosResponse<Event, any>> => {
-    return await api.get<Event>(`/events/${id}`);
+    return await api.get<Event>(`/api/events/${id}`);
 };
