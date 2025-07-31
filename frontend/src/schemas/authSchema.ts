9aa16a173329bb79e13e973be293f4a5d6f862cd
Curtis Wirtz <supcurtis@gmail.com>
2025-07-31 03:58:05 -0700

    replaced old login form with with a better looking one that uses zod and react-hook-form

--- frontend/src/schemas/authSchema.ts
+++ frontend/src/schemas/authSchema.ts
@@ -10,3 +10,10 @@ export const registerSchema = z.object({
 });
 
 export type RegisterFormInputs = z.infer<typeof registerSchema>;
+
+export const loginSchema = z.object({
+  username: z.string().min(5, "Username must be at least 5 characters").max(20, "Username must be less than 20 characters"),
+  password: z.string().min(3, "Password must be at least 3 characters"),
+});
+
+export type LoginFormInputs = z.infer<typeof loginSchema>;
