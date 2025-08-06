c592a6fdee4de369b900db32a57f4d73dfbaf0be
Curtis Wirtz <supcurtis@gmail.com>
2025-08-06 15:58:35 -0700

    successfully switched to Launch UI navbar component

--- frontend/src/components/HeaderSearchBar.tsx
+++ frontend/src/components/HeaderSearchBar.tsx
@@ -6,8 +6,10 @@ import { searchSchema } from '../schemas/searchSchema';
 import type { SearchFormInputs } from '../schemas/searchSchema';
 import { zodResolver } from '@hookform/resolvers/zod';
 
-export const HeaderSearchBar: React.FC = () => {
+export const HeaderSearchBar: React.FC = ({ sheetClose }) => {
   const navigate = useNavigate();
+
+  const SheetClose = sheetClose;
 
   const {
     register,
@@ -54,21 +56,23 @@ export const HeaderSearchBar: React.FC = () => {
       <form onSubmit={handleSubmit(onSubmit, onError)} className="flex items-center relative">
         <input
           type="text"
-          placeholder="Search fighters/events"
+          placeholder="Search for fighters and events by name..."
           {...register("searchQuery")}
           className={`flex-grow p-2 text-sm border rounded-md focus:outline-none focus:ring-1 ${
             errors.searchQuery ? 'border-red-500 focus:ring-red-500' : 'border-gray-300 focus:ring-blue-500'
           }`}
           aria-label="Search"
         />
-        <button
-          type="submit"
-          className="absolute right-2 px-2 py-1.5 bg-gray-800 text-white text-sm rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500 disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"
-          // Button is disabled if input is empty or if it's currently submitting
-          disabled={!currentInputSearchTerm.trim() || !isValid} // isValid from react-hook-form
-        >
-          <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10 6.5C10 8.433 8.433 10 6.5 10C4.567 10 3 8.433 3 6.5C3 4.567 4.567 3 6.5 3C8.433 3 10 4.567 10 6.5ZM9.30884 10.0159C8.53901 10.6318 7.56251 11 6.5 11C4.01472 11 2 8.98528 2 6.5C2 4.01472 4.01472 2 6.5 2C8.98528 2 11 4.01472 11 6.5C11 7.56251 10.6318 8.53901 10.0159 9.30884L12.8536 12.1464C13.0488 12.3417 13.0488 12.6583 12.8536 12.8536C12.6583 13.0488 12.3417 13.0488 12.1464 12.8536L9.30884 10.0159Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
-        </button>
+        <SheetClose asChild>
+          <button
+            type="submit"
+            className="absolute right-1.5 px-2 py-1.5 bg-gray-800 text-white text-sm rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500 disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"
+            // Button is disabled if input is empty or if it's currently submitting
+            disabled={!currentInputSearchTerm.trim() || !isValid} // isValid from react-hook-form
+          >
+            <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10 6.5C10 8.433 8.433 10 6.5 10C4.567 10 3 8.433 3 6.5C3 4.567 4.567 3 6.5 3C8.433 3 10 4.567 10 6.5ZM9.30884 10.0159C8.53901 10.6318 7.56251 11 6.5 11C4.01472 11 2 8.98528 2 6.5C2 4.01472 4.01472 2 6.5 2C8.98528 2 11 4.01472 11 6.5C11 7.56251 10.6318 8.53901 10.0159 9.30884L12.8536 12.1464C13.0488 12.3417 13.0488 12.6583 12.8536 12.8536C12.6583 13.0488 12.3417 13.0488 12.1464 12.8536L9.30884 10.0159Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+          </button>
+        </SheetClose>
       </form>
     </div>
   );
---
+++ frontend/src/components/logos/octagon.tsx
@@ -0,0 +1,13 @@
+const LogoUI = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg fill="#000000" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" 
+	 width={24} height={24} viewBox="0 0 73.935 73.935"
+	 xmlSpace="preserve">
+    <g>
+      <g>
+        <path d="M52.279,73.935H21.656L0,52.279V21.655L21.655,0h30.625l21.655,21.655v30.625L52.279,73.935z M22.898,70.935h28.139
+          l19.898-19.897v-28.14L51.037,2.999H22.898L3,22.897v28.14L22.898,70.935z"/>
+      </g>
+    </g>
+  </svg>
+);
+export default LogoUI;
