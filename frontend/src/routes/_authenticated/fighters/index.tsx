08811e43db4ea8727ab682ee5872975043c12f3c
Curtis Wirtz <supcurtis@gmail.com>
2025-08-08 01:12:18 -0700

    styled fighters index using shadCN theme

--- frontend/src/routes/_authenticated/fighters/index.tsx
+++ frontend/src/routes/_authenticated/fighters/index.tsx
@@ -3,8 +3,12 @@ import { useSuspenseQuery } from '@tanstack/react-query';
 import type { PaginatedResponse } from '../../../api/queries.ts';
 import { getItems } from '../../../api/queries.ts';
 import type { Fighter } from '../../../types/fighter.types.ts';
-import { formatDate } from '../../../lib/dateUtils.ts';
+import { calculateAge, formatDate } from '../../../lib/dateUtils.ts';
 import { getPageParamFromUrl, constructItemsApiUrl } from '../../../lib/urlUtils.ts';
+import { Button } from '@/components/ui/button.tsx';
+import { Section } from "@/components/ui/section";
+import { cn } from "@/lib/utils";
+import { Breadcrumbs } from "@/components/Breadcrumbs";
 
 export const Route = createFileRoute('/_authenticated/fighters/')({
   component: FightersIndex,
@@ -40,8 +44,8 @@ export const Route = createFileRoute('/_authenticated/fighters/')({
       staleTime: 1000 * 60 * 5, // add cache stale time... refresh after 5 mins
     });
   },
-  pendingComponent: () => <div>Loading...</div>,
-  errorComponent: () => <div>Error fetching fighters</div>,
+  pendingComponent: () => <div className="flex justify-center w-full my-20 text-4xl">Loading...</div>,
+  errorComponent: () => <div className="flex justify-center w-full my-20 text-4xl">Error fetching fighters</div>,
 });
 
 
@@ -76,121 +80,163 @@ function FightersIndex() {
   const endItem = Math.min(currentPage * pageSize, totalFighters);
 
   return (
-    <div className="w-full mt-2 items-center bg-gray-100 min-h-screen p-6">
-      <h1 className="text-4xl font-bold mb-6 text-gray-800">Fighters List</h1>
-      <table className="min-w-full bg-white border border-gray-200 shadow-md rounded-lg overflow-hidden">
-        <thead>
-          <tr>
-            <th className="py-3 px-4 border-b text-left text-sm font-semibold text-gray-600">Fighter Name</th>
-            <th className="py-3 px-4 border-b text-center text-sm font-semibold text-gray-600">Weight</th>
-            <th className="py-3 px-4 border-b text-center text-sm font-semibold text-gray-600">Record</th>
-            <th className="py-3 px-4 border-b text-center text-sm font-semibold text-gray-600">DOB</th>
-          </tr>
-        </thead>
-        <tbody>
-          {fighters.length > 0 ? (
-            fighters.map((fighter: Fighter) => {
-              return (
-                <tr key={fighter.fighter_id} className="hover:bg-gray-50 border-b border-gray-200">
-                  <td className='p-3'>
-                    <Link
-                      to="/fighters/$fighterId"
-                      params={{ fighterId: fighter.fighter_id }}
-                      className="text-blue-500 hover:underline flex w-full justify-start"
-                    >
-                      {fighter.name}{fighter.nickname && <span className="italic ml-2">"{fighter.nickname}"</span>}
-                    </Link>
-                  </td>
-                  <td className="whitespace-pre p-3 text-center">
-                    {fighter.weight_lb && <span>{fighter.weight_lb} lbs</span>}
-                  </td>
-                  <td className="whitespace-pre p-3 text-center">
-                    <span>{fighter.wins}-{fighter.losses}-{fighter.draws}</span>
-                  </td>
-                  <td className="whitespace-pre p-3 text-center">
-                    {fighter.date_of_birth && <span className="text-gray-700">{formatDate(fighter.date_of_birth)}</span>}
-                  </td>
-                </tr>
-              );
-            })
-          ) : (
-            <tr>
-              <td colSpan={4} className="p-3 text-center text-gray-500">
-                No fighters found.
-              </td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-
-      {/* Pagination Controls */}
-      {totalFighters > 0 && ( // if there are fighters...
-        <div className="flex flex-col items-center mt-6 space-y-4">
-          {/* Items Counter */}
-          <div className="text-lg font-medium text-gray-700">
-            Displaying {startItem} - {endItem} of {totalFighters} fighters
+    <Section
+      className={cn(
+          "py-0! px-5! max-w-container mx-auto",
+          "",
+    )}>
+      <div className="bg-background w-full mt-6 items-center min-h-screen">
+        <Breadcrumbs />
+        <h1 className="animate-appear text-4xl font-bold mb-6">Fighters List</h1>
+        <div className="w-full animate-appear">
+          <div className="hidden md:grid md:grid-cols-7 tablet:grid-cols-8 border-b border-brand pb-3 gap-3">
+            <div className="col-span-3 text-center text-brand text-2xl">Fighter Name</div>
+            <div className="col-span-1 md:col-span-2 text-center text-brand text-2xl">Record</div>
+            <div className="col-span-1 tablet:col-span-2 text-center text-brand text-2xl">Weight</div>
+            <div className="col-span-1 text-center text-brand text-2xl">Age</div>
           </div>
-
-          {/* Pagination Controls */}
-          <div className="flex justify-center space-x-4 items-center">
-            {/* First Page Link */}
-            {currentPage > 1 && totalPages > 1 ? (
-              <Link
-                to="."
-                search={{ page: 1 }}
-                className="text-blue-600 hover:underline px-3 py-1 rounded-md border border-blue-600 hover:bg-blue-50 text-sm"
-              >
-                {`<< First`}
-              </Link>
+          <div>
+            {fighters.length > 0 ? (
+              fighters.map((fighter: Fighter) => {
+                return (
+                  <div key={fighter.fighter_id} className="md:grid md:grid-cols-7 tablet:grid-cols-8 py-5 tablet:py-3 border-b last:border-0">
+                    <div className="col-span-3 flex flex-col md:flex-row items-center">
+                      <Button asChild variant="secondary" className="group">
+                        <Link
+                          to="/fighters/$fighterId"
+                          params={{ fighterId: fighter.fighter_id }}
+                          className="flex w-fit mx-auto md:w-full justify-center  text-[15px]"
+                        >
+                          {fighter.name}
+                          {fighter.nickname && <span className="italic ml-2">"{fighter.nickname}"</span>}
+                          <span className="ml-2 group-hover:translate-x-2 duration-300 transition-all mb-1 text-brand">&#187;</span>
+                        </Link>
+                      </Button>
+                      <div className="md:hidden flex">
+                        <div className="flex justify-center items-center col-span-1 md:col-span-2 whitespace-pre p-3 text-center text-2xl">
+                          <span>{fighter.wins}-{fighter.losses}-{fighter.draws}</span>
+                        </div>
+                        <div className="col-span-1 tablet:col-span-2 whitespace-pre p-3 text-center text-lg flex items-center justify-center">
+                          {fighter.weight_lb && <span>{fighter.weight_lb} lbs</span>}
+                        </div>
+                        <div className="col-span-1 whitespace-pre p-3 text-center flex flex-col">
+                          {fighter.date_of_birth && (
+                            <>
+                              <span className="text-sm">{formatDate(fighter.date_of_birth)}</span>
+                              <span className="text-sm">{calculateAge(fighter.date_of_birth)} years old</span>
+                            </>
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                    <div className="hidden md:flex justify-center items-center col-span-1 md:col-span-2 whitespace-pre p-3 text-center text-2xl">
+                      <span>{fighter.wins}-{fighter.losses}-{fighter.draws}</span>
+                    </div>
+                    <div className="hidden md:flex col-span-1 tablet:col-span-2 whitespace-pre p-3 text-center text-lg  items-center justify-center">
+                      {fighter.weight_lb && <span>{fighter.weight_lb} lbs</span>}
+                    </div>
+                    <div className="hidden md:flex col-span-1 whitespace-pre p-3 text-center  flex-col">
+                      {fighter.date_of_birth && (
+                        <>
+                          <span className="text-sm">{formatDate(fighter.date_of_birth)}</span>
+                          <span className="text-sm">{calculateAge(fighter.date_of_birth)} years old</span>
+                        </>
+                      )}
+                    </div>
+                  </div>
+                );
+              })
             ) : (
-              <span className="text-gray-400 px-3 py-1 border border-gray-400 rounded-md cursor-not-allowed text-sm">{`<< First`}</span>
-            )}
-
-            {/* Previous Page Link */}
-            {previousPageNumber !== undefined ? (
-              <Link
-                to="."
-                search={{ page: previousPageNumber }}
-                className="text-blue-600 hover:underline px-3 py-1 rounded-md border border-blue-600 hover:bg-blue-50 text-sm"
-              >
-                {`< Prev`}
-              </Link>
-            ) : (
-              <span className="text-gray-400 px-3 py-1 border border-gray-400 rounded-md cursor-not-allowed text-sm">{`< Prev`}</span>
-            )}
-
-            {/* Current Page Indicator */}
-            <span className="text-lg font-medium text-gray-700 mx-2">Page {currentPage} of {totalPages}</span>
-
-            {/* Next Page Link */}
-            {nextPageNumber !== undefined ? (
-              <Link
-                to="."
-                search={{ page: nextPageNumber }}
-                className="text-blue-600 hover:underline px-3 py-1 rounded-md border border-blue-600 hover:bg-blue-50 text-sm"
-              >
-                {`Next >`}
-              </Link>
-            ) : (
-              <span className="text-gray-400 px-3 py-1 border border-gray-400 rounded-md cursor-not-allowed text-sm">{`Next >`}</span>
-            )}    
-
-            {/* Last Page Link */}
-            {currentPage < totalPages && totalPages > 1 ? (
-              <Link
-                to="."
-                search={{ page: totalPages }}
-                className="text-blue-600 hover:underline px-3 py-1 rounded-md border border-blue-600 hover:bg-blue-50 text-sm"
-              >
-                {`Last >>`}
-              </Link>
-            ) : (
-              <span className="text-gray-400 px-3 py-1 border border-gray-400 rounded-md cursor-not-allowed text-sm">{`Last >>`}</span>
+              <div className="p-3 text-center">
+                No fighters found.
+              </div>
             )}
           </div>
         </div>
-      )}
-      
-    </div>
+
+
+        {/* Pagination Controls */}
+        {totalFighters > 0 && ( // if there are fighters...
+          <div className="animate-appear flex flex-col items-center mt-6">
+            {/* Items Counter */}
+            <div className="text-lg text-brand font-medium mb-5">
+              Displaying {startItem} - {endItem} of {totalFighters} events
+            </div>
+
+            {/* Pagination Controls */}
+            <div className="flex justify-center items-center">
+              {/* First Page Link */}
+              {currentPage > 1 && totalPages > 1 ? (
+                <Button asChild>
+                  <Link
+                    to="."
+                    search={{ page: 1 }}
+                  >
+                    {`<<`}
+                  </Link>
+                </Button>
+              ) : (
+                <Button className="cursor-not-allowed opacity-50 mr-2">
+                  {`<<`}
+                </Button>
+              )}
+
+              {/* Previous Page Link */}
+              {previousPageNumber !== undefined ? (
+                <Button asChild>
+                  <Link
+                    to="."
+                    search={{ page: previousPageNumber }}
+                    className="ml-2"
+                  >
+                    {`<`}
+                  </Link>
+                </Button>
+              ) : (
+                <Button className="cursor-not-allowed opacity-50">
+                  {`<`}
+                </Button>
+              )}
+
+              {/* Current Page Indicator */}
+              <span className="text-lg font-medium mx-4">Page {currentPage} of {totalPages}</span>
+
+              {/* Next Page Link */}
+              {nextPageNumber !== undefined ? (
+                <Button asChild>
+                  <Link
+                    to="."
+                    search={{ page: nextPageNumber }}
+                    className="mr-2"
+                  >
+                    {`>`}
+                  </Link>
+                </Button>
+              ) : (
+                <Button asChild>
+                  {`>`}
+                </Button>
+              )}
+
+              {/* Last Page Link */}
+              {currentPage < totalPages && totalPages > 1 ? (
+                <Button asChild>
+                  <Link
+                    to="."
+                    search={{ page: totalPages }}
+                  >
+                    {`>>`}
+                  </Link>
+                </Button>
+              ) : (
+                <Button asChild>
+                  {`>>`}
+                </Button>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
+    </Section>
   );
 }
