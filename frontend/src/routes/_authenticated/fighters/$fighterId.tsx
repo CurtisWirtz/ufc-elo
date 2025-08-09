f7c86e88c40cb6018ae00e9a35e2c16d7fa01a7b
Curtis Wirtz <supcurtis@gmail.com>
2025-08-09 12:21:43 -0700

    ind. fighters mobile styling

--- frontend/src/routes/_authenticated/fighters/$fighterId.tsx
+++ frontend/src/routes/_authenticated/fighters/$fighterId.tsx
@@ -72,7 +72,7 @@ function FighterPage() {
               <span className="text-2xl text-brand">Stance:</span>
               {fighter.data.stance ? <span>{fighter.data.stance}</span> : <span>N/A</span>}
             </div>
-            <div className="col-span-1 flex flex-col items-center">
+            <div className="col-span-1 flex flex-col items-center text-center">
               <span className="text-2xl text-brand">Age:</span>
               {fighter.data.date_of_birth ? (
                 <>
@@ -91,29 +91,28 @@ function FighterPage() {
 
           {fighter.data?.participated_bouts && fighter.data?.participated_bouts.length > 0 && (
             isFutureDate(fighter.data?.participated_bouts[0].event.date) && (
-              <div className="border-1 p-4 rounded mb-6 text-center flex flex-col">
+              <div className="border-1 border-brand p-4 rounded mb-6 text-center flex flex-col items-center">
                 <p className="text-2xl text-brand mb-2">Upcoming Bout:</p>
-                <span className="mb-2">{formatDate(fighter.data?.participated_bouts[0].event.date)} - {fighter.data?.participated_bouts[0].event.location}</span>
-                <p className="text-md">
-                  <Button asChild variant="secondary" className="group w-min">
-                    <Link to={`/events/${fighter.data?.participated_bouts[0].event.event_id}`}>
-                      {fighter.data?.participated_bouts[0].event.name}
-                      <span className="ml-2 group-hover:translate-x-1 duration-300 transition-all text-brand">&#187;</span>
-                    </Link>
-                  </Button>
-                </p>
-                <span className="mt-6 mb-2">Opponent: </span>
+                <span className="">{formatDate(fighter.data?.participated_bouts[0].event.date)}</span>
+                <span className="">{fighter.data?.participated_bouts[0].event.location}</span>
+                <Button asChild variant="secondary" className="group w-min mt-2">
+                  <Link to={`/events/${fighter.data?.participated_bouts[0].event.event_id}`}>
+                    {fighter.data?.participated_bouts[0].event.name}
+                    <span className="ml-2 group-hover:translate-x-1 duration-300 transition-all text-brand">&#187;</span>
+                  </Link>
+                </Button>
+                <span className="text-xl mt-5 mb-2 text-brand">Opponent: </span>
                 <p className="">
                   {(fighter.data?.participated_bouts[0].fighter_1.fighter_id === fighter.data.fighter_id) ? (
                     <Button asChild variant="secondary" className="group w-min">
-                      <Link className="text-blue-500 hover:underline" to={`/fighters/${fighter.data?.participated_bouts[0].fighter_2.fighter_id}`}>
+                      <Link to={`/fighters/${fighter.data?.participated_bouts[0].fighter_2.fighter_id}`}>
                         {fighter.data?.participated_bouts[0].fighter_2.name}
                         <span className="ml-2 group-hover:translate-x-1 duration-300 transition-all text-brand">&#187;</span>
                       </Link>
                     </Button>
                   ) : (
                     <Button asChild variant="secondary" className="group w-min">
-                      <Link className="text-blue-500 hover:underline" to={`/fighters/${fighter.data?.participated_bouts[0].fighter_1.fighter_id}`}>
+                      <Link to={`/fighters/${fighter.data?.participated_bouts[0].fighter_1.fighter_id}`}>
                         {fighter.data?.participated_bouts[0].fighter_1.name}
                         <span className="ml-2 group-hover:translate-x-1 duration-300 transition-all text-brand">&#187;</span>
                       </Link>
@@ -125,7 +124,7 @@ function FighterPage() {
           )}
 
           <h1 className="text-3xl mb-5 text-brand text-center">Bout History</h1>
-          <div className="hidden md:grid grid-cols-12 border-b border-brand">
+          <div className="hidden tablet:grid grid-cols-12 border-b border-brand">
             <div className="col-span-4 py-3 px-4 text-center text-brand text-lg">Opponent</div>
             <div className="col-span-4 py-3 px-4 text-center text-brand text-lg">Event</div>
             <div className="col-span-2 py-3 px-4 text-center text-brand text-lg">Method</div>
@@ -137,8 +136,8 @@ function FighterPage() {
               {fighter.data?.participated_bouts.map((bout) => {
                 if (!isFutureDate(bout.event.date)) {
                   return (
-                    <div key={bout.bout_id} className="py-5 tablet:py-3 md:grid grid-cols-12 border-b last:border-0">
-                      <div className="col-span-4 text-center">
+                    <div key={bout.bout_id} className="py-5 tablet:py-3 tablet:grid grid-cols-12 border-b last:border-0">
+                      <div className="tablet:col-span-4 text-center">
                         {bout.winning_fighter ? 
                           (fighter.data.fighter_id === bout.winning_fighter.fighter_id) ? (
                             <span className="border-2 border-brand text-brand rounded-sm px-2 py-1.5 mr-2">
@@ -168,22 +167,37 @@ function FighterPage() {
                           </Button>
                         )}
                         {bout.details && <div className="text-sm font-normal mt-2">{bout.details}</div>}
+                        {bout.referee && <span className="flex tablet:hidden justify-center text-sm mt-1 text-brand">Referee: {bout.referee}</span>}
                       </div>
-                      <div className="col-span-4 text-center flex flex-col items-center justify-center">
-                        <Button asChild variant="secondary" className="group w-min">
+                      <div className="flex justify-between tablet:hidden my-2 max-w-80 mx-auto">
+                        <div className="text-center flex flex-col items-center">
+                          <span className="text-brand">Method:</span>
+                          <span className="text-sm">{bout.method}</span>
+                        </div>
+                        <div className="text-center flex flex-col items-center">
+                          <span className="text-brand">Round:</span>
+                          {bout.ending_round}
+                        </div>
+                        <div className="text-center flex flex-col items-center">
+                          <span className="text-brand">Time:</span>
+                          {bout.ending_time}
+                        </div>
+                      </div>
+                      <div className="tablet:col-span-4 text-center flex flex-col items-center justify-center">
+                        <Button asChild variant="secondary" className="group w-min order-2 tablet:order-none">
                           <Link to={`/events/${bout.event.event_id}`}>
                             {bout.event.name}
                             <span className="ml-2 group-hover:translate-x-1 duration-300 transition-all text-brand">&#187;</span>
                           </Link>
                         </Button>
-                        <span className="mt-2">{formatDate(bout.event.date)}</span>
+                        <span className="mt-2 order-1 tablet:order-none">{formatDate(bout.event.date)}</span>
                       </div>
-                      <div className="col-span-2 text-center flex flex-col items-center justify-center">
+                      <div className="hidden tablet:col-span-2 text-center tablet:flex flex-col items-center justify-center">
                         <span className="text-sm">{bout.method}</span>
                         {bout.referee && <span className="text-sm mt-2">Referee: {bout.referee}</span>}
                       </div>
-                      <div className="col-span-1 text-center flex items-center justify-center">{bout.ending_round}</div>
-                      <div className="col-span-1 text-center flex items-center justify-center">{bout.ending_time}</div>
+                      <div className="hidden tablet:col-span-1 text-center tablet:flex items-center justify-center">{bout.ending_round}</div>
+                      <div className="hidden tablet:col-span-1 text-center tablet:flex items-center justify-center">{bout.ending_time}</div>
                     </div>
                   );
                 }
